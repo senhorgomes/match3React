72e5e3012abfe093f72939fbd9770f2a56e3f4eb
Bryan's Mac <bryanpgomes@gmail.com>
2022-03-02 22:35:19 -0500

    Creating checkForMatches functions

---
+++ src/components/CheckForMatches/CheckForMatchesRows/checkForMatchesRows.js
@@ -0,0 +1,7 @@
+export function checkForMacthesRowsThree (array) {
+  for(let i = 0; i < 64; i++) {
+    const threeOfAKind = [i, i + 1, i + 2];
+    const selectedColor = array[i];
+    const notValidLocation = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+  }
+}
